192ee2182b6ff3b1ab389474a53b86483adbcfae
Ahirkar <140475764+Ahirkar@users.noreply.github.com>
2023-08-18 12:31:33 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -6,7 +6,7 @@ package com.companyname.bank;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] hhargs )
     {
         System.out.println( "scm okkkk");
     }
